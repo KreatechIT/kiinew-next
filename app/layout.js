25a76039515dae2b35682ce303855f2992974b3f
mehmetfaahem <mehmetfaahem@gmail.com>
2024-04-29 17:06:49 +0600

    headed

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} w-screen h-screen`}>{children}</body>
+      <body className={`${inter.className} w-screen h-screen`}>
+        <section className="flex justify-center items-center">
+          <Header />
+        </section>
+        <section className="px-[5%] py-[5%]">{children}</section>
+      </body>
     </html>
   );
 }
---
+++ components/Header.js
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+function Header() {
+  return (
+    <div className="flex w-full p-[20px]">
+      <div>
+        {/* <Image
+          alt="Logo"
+          src={`https://jrenterprise.com.bd/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fjr-logo-white.e0360d2b.png&w=384&q=75`}
+          width={200}
+          height={200}
+        /> */}
+      </div>
+      <div>
+        <div className="w-[500px] pl-[10px] justify-between px-[6px] flex place-content-center place-items-center h-[60px] border-2 border-black rounded-[35px]">
+          <input placeholder="Search" className="w-auto p-[3px] outline-none" />
+          <button className="p-[3px] text-[20px] w-[120px] rounded-[25px] text-white h-[45px] bg-[#FC7457]">
+            SEARCH
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
